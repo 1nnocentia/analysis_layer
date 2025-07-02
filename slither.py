555862c6200772023a4f0f2033a2bac4a9fa3cb3
Innocentia <innocentiahan@gmail.com>
2025-07-02 09:00:36 +0800

    compleating basemodel and instalize fast api

--- slither.py
+++ slither.py
@@ -15,4 +15,39 @@ from typing import List # variable type untuk list
 class ContractInput(BaseModel):
     """
     Model untuk data input
+    Cek apakah JSON yang diterima bertipe String
     """
+    source_code: str
+
+class Issue(BaseModel):
+    """
+    Model untuk issue yang ditemukan
+    @param
+    type: jenis issue (string)
+    line: nomor baris issue ditemukan (int)
+    severity: tingkat keparahan issue (string)
+    message: pesan deskriptif tentang issue (string)
+    """
+    type: str
+    line: int
+    severity: str
+    message: str
+
+class AnalysisResponse(BaseModel):
+    """
+    Model untuk respon JSON yang dikirim kembali
+    @param
+    issues: daftar issue yang ditemukan (list of Issue)
+    """
+    issues: List[Issue]
+
+# 2. Initialize FastAPI
+
+app = FastAPI(
+    title="Slither Analysis Service",
+    desciption="API untuk analisis statis smart contract dengan slither",
+    version="1.0.0"
+)
+
+# 3. Helper untuk transformasi output
+

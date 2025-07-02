f2ba8356943f8c90a97b05b400d03eb9bb9cb99e
Innocentia <innocentiahan@gmail.com>
2025-07-02 11:39:52 +0800

    data input and api initialization

--- analysis_service_02.py
+++ analysis_service_02.py
@@ -13,3 +13,24 @@ from pydantic import BaseModel, Field
 from typing import List
 import requests 
 
+# 1. Input data sesuai output dari layer 1
+class Issue(BaseModel):
+    type: str
+    line: int
+    severity: str
+    message: str
+
+class AnalysisInput(BaseModel):
+    risk_summary: str = Field(..., description="Ringkasan risiko menyeluruh secara singkat")
+    llm_recomendation: str = Field(..., description="Rekomendasi perbaikan")
+    risk_grading: str = Field(..., description="Grading risiko menyeluruh")
+    confidence_score: float = Field(..., description="Confidence score analisa")
+
+#2. Initialize FastAPI
+
+app = FastAPI(
+    title="LLM Analysis Service",
+    description="Layer Analisis kedua untuk laporan analisa",
+    version="1.0.0",
+)
+

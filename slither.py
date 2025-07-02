cfe1f5b36060eea4691b27845fd45d1281871c29
Innocentia <innocentiahan@gmail.com>
2025-07-02 09:19:41 +0800

    creat transformation output

--- slither.py
+++ slither.py
@@ -42,12 +42,46 @@ class AnalysisResponse(BaseModel):
     issues: List[Issue]
 
 # 2. Initialize FastAPI
+# akan digunakan untuk membuat endpoint API
 
 app = FastAPI(
     title="Slither Analysis Service",
-    desciption="API untuk analisis statis smart contract dengan slither",
+    description="API untuk analisis statis smart contract dengan slither",
     version="1.0.0"
 )
 
 # 3. Helper untuk transformasi output
 
+def format_slither_output(slither_row_json: dict) -> List[Issue]:
+    """
+    Mengubah output JSON dari Slither menjadi format sesuai dengan input di layer setelahnya
+    """
+    formated_issues = []
+
+    # validasi apakah analisis berhasil
+    if not slither_row_json.get("success") or not slither_row_json.get("result", {}).get("detectors"):
+        return []
+    
+    detectors = slither_row_json["result"]["detectors"]
+
+    for detector in detectors:
+        # informasi dasar
+        issue_type = detector.get("check", "N/A")
+        severity = detector.get("impact", "N/A")
+        # ambil infromasi dari Slither
+        message = detector.get("message", "No message available").split("\n")[0]
+
+        # detektor bisa punya beberapa lokasi
+        if "elements" in detector:
+            for element in detector["elements"]:
+                # ambil nomor baris dari elemen
+                line_number = element.get("source_mapping", {}).get("start", {}).get("line", [0])[0]
+                
+                issue = Issue(
+                    type=issue_type,
+                    line=line_number,
+                    severity=severity,
+                    message=message
+                )
+                formated_issues.append(issue)
+    return formated_issues
